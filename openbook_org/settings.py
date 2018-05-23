301567aa93178e14aef6efb4d1d99d4840458c49
Joel Hernandez <lifenautjoe@gmail.com>
2018-05-23 22:22:58 +0200

    🔇 only enable sentry in prod

--- openbook_org/settings.py
+++ openbook_org/settings.py
@@ -63,12 +63,13 @@ AUTH_PASSWORD_VALIDATORS = []
 
 # Sentry Config
 
-RAVEN_CONFIG = {
-    'dsn': os.environ.get('SENTRY_DSN'),
-    # If you are using git, you can also automatically configure the
-    # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
-}
+if IS_PRODUCTION_ENVIRONMENT:
+    RAVEN_CONFIG = {
+        'dsn': os.environ.get('SENTRY_DSN'),
+        # If you are using git, you can also automatically configure the
+        # release based on the git info.
+        'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    }
 
 # Mailgun config
 
