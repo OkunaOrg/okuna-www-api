ab607a43af25a8059a690cda99d4e47655e0e4ce
Joel Hernandez <lifenautjoe@gmail.com>
2018-05-23 19:42:05 +0200

    ⬆️ add django rest framework

--- openbook_org/settings.py
+++ openbook_org/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.contenttypes'
+    'django.contrib.contenttypes',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
