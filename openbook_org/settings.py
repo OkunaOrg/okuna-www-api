9bc4bd9a8e941b63796076f1c55408a357a1fb73
Joel Hernandez <joel@open-book.org>
2020-09-13 13:49:33 +0200

    :fire: remove database entirely

--- openbook_org/settings.py
+++ openbook_org/settings.py
@@ -75,10 +75,6 @@ WSGI_APPLICATION = 'openbook_org.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    }
 }
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
